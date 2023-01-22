253bce7018a7919afa8f5451c8a13b4d30c10247
hazem-ali <hazemali627@gmail.com>
2023-01-22 11:32:06 +0200

    about and service views

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-from .views import (ListContactDataAPIView, ContactDataAPIView)
+from .views import (ListContactDataAPIView, ContactDataAPIView,
+                    ListAboutAPIView, AboutAPIView, ListServiceAPIView, ServiceAPIView)
 
 from rest_framework.routers import DefaultRouter
 
@@ -8,9 +9,18 @@ router = DefaultRouter()
 # router.register('contacts', ContactDataViewSet, basename='members-list')
 
 
-
 urlpatterns = [
-    path('contacts/<int:user>', ContactDataAPIView.as_view(), name='contact-data-item'),
-    # path('contacts/', ListContactDataAPIView.as_view(), name='contact-data-list'),
+    path('contacts/', ListContactDataAPIView.as_view(), name='contact-data-list'),
+    path('contacts/<int:user>', ContactDataAPIView.as_view(),
+         name='contact-data-item'),
+
+
+    path('about/', ListAboutAPIView.as_view(), name='about-list'),
+    path('about/<int:user>', AboutAPIView.as_view(),
+         name='about-item'),
+
+    path('services/', ListServiceAPIView.as_view(), name='service-list'),
+    path('services/<int:user>', ServiceAPIView.as_view(),
+         name='service-item'),
     # path('', include(router.urls)),
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,21 +1,35 @@
 from django.shortcuts import render
-from rest_framework import viewsets, generics,views
+from rest_framework import viewsets, generics, views
 from rest_framework.response import Response
 
+from rest_framework import status
 from .models import (ContactData, About, Service)
-from .serializers import (ContactDataSerializer, AboutSerializer, ServiceSerializer)
-
+from .serializers import (ContactDataSerializer,
+                          AboutSerializer, ServiceSerializer)
 
 
 # class ContactDataViewSet(viewsets.ModelViewSet):
 #     queryset = ContactData.objects.all()
 #     serializer_class = ContactDataSerializer
-    # def retrieve(self, request, *args, **kwargs):
-    #     user_pk = kwargs.get('pk')
-    #     contact_data = ContactData.objects.get(pk=user_pk)
-    #     serializer = ContactDataSerializer(contact_data)
-    #     return Response(serializer.data)
-    
+# def retrieve(self, request, *args, **kwargs):
+#     user_pk = kwargs.get('pk')
+#     contact_data = ContactData.objects.get(pk=user_pk)
+#     serializer = ContactDataSerializer(contact_data)
+#     return Response(serializer.data)
+
+
+
+# class ContactDataAPI(generics.GenericAPIView):
+#     queryset = ContactData.objects.all()
+#     serializer_class = ContactDataSerializer
+#     def get_object(self):
+#         user_pk = self.kwargs.get('user')
+#         print(user_pk)
+
+#         contact_data = ContactData.objects.get(user=user_pk)
+#         serializer = ContactDataSerializer(contact_data)
+#         return Response(serializer.data)
+
 
 class ListContactDataAPIView(generics.ListCreateAPIView):
     queryset = ContactData.objects.all()
@@ -23,16 +37,55 @@ class ListContactDataAPIView(generics.ListCreateAPIView):
 
 
 class ContactDataAPIView(views.APIView):
-    
 
-    def get(self, request,user):
-        user_pk = self.kwargs['user']
-        print(user_pk)
-        print(user)
+    def get(self, request, user):
+
         contact_data = ContactData.objects.get(user=user)
         serializer = ContactDataSerializer(contact_data)
         return Response(serializer.data)
 
+    def delete(self, request, user):
+
+        contact_data = ContactData.objects.get(user=user)
+        contact_data.delete()
+        return Response({'message': 'Deleted Successfully'}, status=status.HTTP_204_NO_CONTENT)
 
 
-    
+class ListAboutAPIView(generics.ListCreateAPIView):
+    queryset = About.objects.all()
+    serializer_class = AboutSerializer
+
+
+class AboutAPIView(views.APIView):
+
+    def get(self, request, user):
+
+        about_details = About.objects.get(user=user)
+        serializer = AboutSerializer(about_details)
+        return Response(serializer.data)
+
+    def delete(self, request, user):
+
+        about_details = About.objects.get(user=user)
+        about_details.delete()
+        return Response({'message': 'Deleted Successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+
+class ListServiceAPIView(generics.ListCreateAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
+
+class ServiceAPIView(views.APIView):
+
+    def get(self, request, user):
+
+        service_details = Service.objects.get(user=user)
+        serializer = ServiceSerializer(service_details)
+        return Response(serializer.data)
+
+    def delete(self, request, user):
+
+        service_details = Service.objects.get(user=user)
+        service_details.delete()
+        return Response({'message': 'Deleted Successfully'}, status=status.HTTP_204_NO_CONTENT)
